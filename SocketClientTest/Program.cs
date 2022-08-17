ca1c7d7448442415c8e3be7c33bde5d7fa836d00
loxloxloxlox <senja2018@mail.ru>
2022-08-17 20:48:43 +0300

    Добавьте файлы проекта.

---
+++ SocketClientTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3003);
+
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+// подключаемся к удаленному хосту
+socket.Connect(ipPoint);
+Console.Write("Введите сообщение:");
+var message = Console.ReadLine();
+var data = Encoding.Unicode.GetBytes(message);
+socket.Send(data);
---
+++ SocketServer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var connections = new Dictionary<int, Socket>();
+var ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3003);
+var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+listener.Bind(ipPoint);
+listener.Listen(1000);
+
+Console.WriteLine("Start...");
+
+while (true)
+{
+    
+
+    var acceptThread = new Thread( () =>
+    {
+        var clientSocket = listener.Accept();
+
+        //var connectionThread = new Thread(() => handleClient(clientSocket));
+        handleClient(clientSocket);
+        //connectionThread.Start();
+
+    });
+    acceptThread.Start();
+
+    // acceptAsync new Thread
+    var res = Console.ReadLine();
+    Console.WriteLine(res);
+}
+
+void handleClient(Socket socket)
+{
+    connections.Add(connections.Count + 1, socket);
+    var builder = new StringBuilder();
+    int bytes = 0; // количество полученных байтов
+    var data = new byte[256]; // буфер для получаемых данных
+
+    do
+    {
+        bytes = socket.Receive(data);
+        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+    }
+    while (socket.Available > 0);
+
+    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+}
